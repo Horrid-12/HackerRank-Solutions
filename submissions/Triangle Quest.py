a4d76c3d2df9ba78f8d4a28f97911e997d654f63
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-14 00:50:14 +0530

    added submissions/Triangle Quest.py

---
+++ submissions/Triangle Quest.py
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Triangle Quest
+Problem Link: /challenges/python-quest-1
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
