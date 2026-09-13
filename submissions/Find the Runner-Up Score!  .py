3bdab651d8ee8852a16259b48d3820041e650789
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-14 00:50:23 +0530

    added submissions/Find the Runner-Up Score!  .py

---
+++ submissions/Find the Runner-Up Score!  .py
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Find the Runner-Up Score!  
+Problem Link: /challenges/find-second-maximum-number-in-a-list
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
