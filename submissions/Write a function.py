5300f786c84c8914b56b70f120526472e76c7d12
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-14 00:50:28 +0530

    added submissions/Write a function.py

---
+++ submissions/Write a function.py
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Write a function
+Problem Link: /challenges/write-a-function
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
