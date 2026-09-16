51ee7b762a22adf08f920b74c487ac1709f88f86
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-16 23:12:56 +0530

    added submissions/Basic Data Types.cpp

---
+++ submissions/Basic Data Types.cpp
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Basic Data Types
+Problem Link: /challenges/c-tutorial-basic-data-types
+Author: Horrid-12
+Language: cpp
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
