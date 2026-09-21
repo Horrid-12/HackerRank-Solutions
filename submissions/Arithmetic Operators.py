4cb3ce6356db53a88797d720d930fdc8b636ad58
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:04:04 +0530

    added submissions/Arithmetic Operators.py

---
+++ submissions/Arithmetic Operators.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Arithmetic Operators
+Problem Link: /challenges/python-arithmetic-operators
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
