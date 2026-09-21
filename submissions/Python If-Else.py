166757dd930a6222de352c8476de56d1e32e5b28
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:04:02 +0530

    added submissions/Python If-Else.py

---
+++ submissions/Python If-Else.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Python If-Else
+Problem Link: /challenges/py-if-else
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
