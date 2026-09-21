0d30248d4386e44014b551dc74f6e2811c1424ff
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:44 +0530

    added submissions/What's Your Name?.py

---
+++ submissions/What's Your Name?.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: What's Your Name?
+Problem Link: /challenges/whats-your-name
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
