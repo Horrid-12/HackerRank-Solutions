74418c5f56b4b2a99073a83933de7fb1b06718f9
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:43 +0530

    added submissions/Lists.py

---
+++ submissions/Lists.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Lists
+Problem Link: /challenges/python-lists
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
