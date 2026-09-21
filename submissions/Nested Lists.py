a03b7722c52fdfe5c2eec3a17cab037053c390b8
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:55 +0530

    added submissions/Nested Lists.py

---
+++ submissions/Nested Lists.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Nested Lists
+Problem Link: /challenges/nested-list
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
