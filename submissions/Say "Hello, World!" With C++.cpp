452746605be518d9f263400eadc0332a4dea8200
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:42 +0530

    added submissions/Say "Hello, World!" With C++.cpp

---
+++ submissions/Say "Hello, World!" With C++.cpp
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Say "Hello, World!" With C++
+Problem Link: /challenges/cpp-hello-world
+Author: Horrid-12
+Language: cpp
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
