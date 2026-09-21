1d5680552737b55e2aaaa02ddcafb93a7ad9b932
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:45 +0530

    added submissions/List Comprehensions.py

---
+++ submissions/List Comprehensions.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: List Comprehensions
+Problem Link: /challenges/list-comprehensions
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
