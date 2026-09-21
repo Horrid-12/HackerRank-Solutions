026300084e1eea6e5a9256c69f34d4516dcc14b0
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:40 +0530

    added submissions/Input and Output.cpp

---
+++ submissions/Input and Output.cpp
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Input and Output
+Problem Link: /challenges/cpp-input-and-output
+Author: Horrid-12
+Language: cpp
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
