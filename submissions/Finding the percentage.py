fd84ef8f80b5d8fff79ad7160a94564458f23bec
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-21 20:03:54 +0530

    added submissions/Finding the percentage.py

---
+++ submissions/Finding the percentage.py
@@ -0,0 +1,11 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Finding the percentage
+Problem Link: /challenges/finding-the-percentage
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+// Could not fetch code snippet
