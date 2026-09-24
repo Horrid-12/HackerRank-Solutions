70b18ec6dd4e41cfe9b4f22a33ee997cabc70d2f
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-24 20:52:30 +0530

    added submissions/Arrays.py

---
+++ submissions/Arrays.py
@@ -0,0 +1,14 @@
+'''-----------------------------------------------------------------------
+
+Problem Title: Arrays
+Problem Link: /challenges/np-arrays
+Author: Horrid-12
+Language: pypy3
+
+-----------------------------------------------------------------------'''
+
+
+
+def arrays(arr):
+
+ return numpy.array(arr[::-1], float)
