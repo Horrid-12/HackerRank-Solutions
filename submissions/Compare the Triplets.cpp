6a52deed0f545f3c32fa306c17ae6bdaa4f5afe2
Swar Gandhi <96373496+Horrid-12@users.noreply.github.com>
2026-09-24 20:52:32 +0530

    added submissions/Compare the Triplets.cpp

---
+++ submissions/Compare the Triplets.cpp
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------
+
+Problem Title: Compare the Triplets
+Problem Link: /challenges/compare-the-triplets
+Author: Horrid-12
+Language: cpp
+
+-----------------------------------------------------------------------*/
+
+
+// Could not fetch code snippet
